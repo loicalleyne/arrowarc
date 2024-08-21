d41645c3b8365b851ba35a24e30960de8a989989
TFMV <mcgeehan@gmail.com>
2024-08-21 17:11:58 -0500

    Add CSV and Parquet Conversion Tools

---
+++ cmd/csv_to_parquet/main.go
@@ -0,0 +1,88 @@
+// --------------------------------------------------------------------------------
+// Author: Thomas F McGeehan V
+//
+// This file is part of a software project developed by Thomas F McGeehan V.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+// For more information about the MIT License, please visit:
+// https://opensource.org/licenses/MIT
+//
+// Acknowledgment appreciated but not required.
+// --------------------------------------------------------------------------------
+
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	converter "github.com/ArrowArc/ArrowArc/pkg/convert"
+	"github.com/apache/arrow/go/v17/arrow"
+	"github.com/docopt/docopt-go"
+)
+
+func main() {
+	usage := `CSV to Parquet Converter.
+
+Usage:
+  csv_to_parquet --csv=<csv_file> --parquet=<parquet_file> [--header=<true|false>] [--chunk-size=<bytes>] [--delimiter=<char>] [--null=<value>] [--strings-can-be-null=<true|false>]
+  csv_to_parquet -h | --help
+
+Options:
+  -h --help                             Show this screen.
+  --csv=<csv_file>                      Path to the input CSV file.
+  --parquet=<parquet_file>              Path to the output Parquet file.
+  --header=<true|false>                 Indicates if the CSV file has a header [default: true].
+  --chunk-size=<bytes>                  Number of bytes to read per chunk [default: 1024].
+  --delimiter=<char>                    Delimiter used in the CSV file [default: ,].
+  --null=<value>                        Value representing null in the CSV file [default: NULL].
+  --strings-can-be-null=<true|false>    Indicates if strings can be null [default: true].
+`
+
+	arguments, err := docopt.ParseDoc(usage)
+	if err != nil {
+		log.Fatalf("Error parsing arguments: %v", err)
+	}
+
+	csvPath, _ := arguments.String("--csv")
+	parquetPath, _ := arguments.String("--parquet")
+	hasHeader, _ := arguments.Bool("--header")
+	chunkSize, _ := arguments.Int("--chunk-size")
+	delimiter, _ := arguments.String("--delimiter")
+	nullValue, _ := arguments.String("--null")
+	stringsCanBeNull, _ := arguments.Bool("--strings-can-be-null")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Minute)
+	defer cancel()
+
+	// Define the schema for the CSV file (customize based on actual CSV structure)
+	schema := arrow.NewSchema([]arrow.Field{
+		{Name: "id", Type: arrow.PrimitiveTypes.Int64, Nullable: false},
+		{Name: "name", Type: arrow.BinaryTypes.String, Nullable: true},
+	}, nil)
+
+	err = converter.ConvertCSVToParquet(ctx, csvPath, parquetPath, schema, hasHeader, int64(chunkSize), rune(delimiter[0]), []string{nullValue}, stringsCanBeNull)
+	if err != nil {
+		log.Fatalf("Error converting CSV to Parquet: %v", err)
+	}
+
+	log.Println("CSV to Parquet conversion completed successfully")
+}
---
+++ tests/convert_csv_to_parquet_test.go
@@ -0,0 +1,130 @@
+// --------------------------------------------------------------------------------
+// Author: Thomas F McGeehan V
+//
+// This file is part of a software project developed by Thomas F McGeehan V.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+// For more information about the MIT License, please visit:
+// https://opensource.org/licenses/MIT
+//
+// Acknowledgment appreciated but not required.
+// --------------------------------------------------------------------------------
+
+package test
+
+import (
+	"context"
+	"os"
+	"testing"
+	"time"
+
+	converter "github.com/ArrowArc/ArrowArc/pkg/convert"
+	"github.com/apache/arrow/go/v17/arrow"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestConvertCSVToParquet(t *testing.T) {
+	t.Parallel() // Parallelize the top-level test
+
+	// Generate a sample CSV file for testing with header
+	csvFilePathWithHeader := "sample_test_with_header.csv"
+	schema := arrow.NewSchema([]arrow.Field{
+		{Name: "id", Type: arrow.PrimitiveTypes.Int64, Nullable: false},
+		{Name: "name", Type: arrow.BinaryTypes.String, Nullable: true},
+	}, nil)
+
+	csvContentWithHeader := `id,name
+1,John
+2,Jane
+3,Jack`
+
+	err := os.WriteFile(csvFilePathWithHeader, []byte(csvContentWithHeader), 0644)
+	assert.NoError(t, err, "Error should be nil when generating CSV file with header")
+
+	// Generate a sample CSV file for testing without header
+	csvFilePathWithoutHeader := "sample_test_without_header.csv"
+	csvContentWithoutHeader := `1,John
+2,Jane
+3,Jack`
+
+	err = os.WriteFile(csvFilePathWithoutHeader, []byte(csvContentWithoutHeader), 0644)
+	assert.NoError(t, err, "Error should be nil when generating CSV file without header")
+
+	// Use t.Cleanup to ensure the files are removed after all tests complete
+	t.Cleanup(func() {
+		os.Remove(csvFilePathWithHeader)
+		os.Remove(csvFilePathWithoutHeader)
+	})
+
+	tests := []struct {
+		csvFilePath      string
+		parquetFilePath  string
+		schema           *arrow.Schema
+		hasHeader        bool
+		chunkSize        int64
+		delimiter        rune
+		nullValues       []string
+		stringsCanBeNull bool
+		description      string
+	}{
+		{
+			csvFilePath:      csvFilePathWithHeader,
+			parquetFilePath:  "output_test_1.parquet",
+			schema:           schema,
+			hasHeader:        true,
+			chunkSize:        1024,
+			delimiter:        ',',
+			nullValues:       []string{"", "NULL"},
+			stringsCanBeNull: true,
+			description:      "Convert CSV to Parquet with header",
+		},
+		{
+			csvFilePath:      csvFilePathWithoutHeader,
+			parquetFilePath:  "output_test_2.parquet",
+			schema:           schema,
+			hasHeader:        false,
+			chunkSize:        2048,
+			delimiter:        ',',
+			nullValues:       []string{"", "NULL"},
+			stringsCanBeNull: true,
+			description:      "Convert CSV to Parquet without header",
+		},
+	}
+
+	for _, test := range tests {
+		test := test // capture range variable
+		t.Run(test.description, func(t *testing.T) {
+			t.Parallel() // Parallelize each subtest
+
+			ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+			defer cancel()
+
+			err := converter.ConvertCSVToParquet(ctx, test.csvFilePath, test.parquetFilePath, test.schema, test.hasHeader, test.chunkSize, test.delimiter, test.nullValues, test.stringsCanBeNull)
+			assert.NoError(t, err, "Error should be nil when converting CSV to Parquet")
+
+			_, err = os.Stat(test.parquetFilePath)
+			assert.NoError(t, err, "Parquet file should be created")
+
+			t.Cleanup(func() {
+				os.Remove(test.parquetFilePath)
+			})
+		})
+	}
+}
