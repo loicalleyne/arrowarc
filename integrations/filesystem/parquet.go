e81597b8c828347e61c66b4af774518c6d2ce53f
TFMV <mcgeehan@gmail.com>
2024-08-21 13:43:44 -0500

    Filesystem and DuckDB Integration Debugging Cleanup

--- integrations/filesystem/parquet.go
+++ integrations/filesystem/parquet.go
@@ -27,7 +27,7 @@
 // Acknowledgment appreciated but not required.
 // --------------------------------------------------------------------------------
 
-package file
+package integrations
 
 import (
 	"context"
